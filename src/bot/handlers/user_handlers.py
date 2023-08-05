0ade463d313c31b7c6df3c9362468e8543d40810
Vi <380673172158vv@gmail.com>
2023-08-06 00:17:30 +0300

    Added cache and fix button generation a bit

--- src/bot/handlers/user_handlers.py
+++ src/bot/handlers/user_handlers.py
@@ -3,16 +3,10 @@ from aiogram import F, filters
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
-# from src.bot.buttons import user_btn as answers
 from src.bot.data import answers
 from src.bot.data.answers import buttons, messages
-
-# from src.bot.data.language import translations
 from src.bot.setup import user_router
 from src.bot.utils.cache import Cache
-
-# from src.db.home.crud import crud_user
-# from src.db.home.crud import crud_user
 from src.db.home.crud import CRUD_User
 from src.db.home.tables import User
 
@@ -23,20 +17,21 @@ cache = Cache()
 @user_router.message(F.text.in_(buttons["cancel"].values()))
 async def cancel_handler(message: t.Message, state: FSMContext) -> None:
     current_state = await state.get_state()
+    user = await cache.get_user(message)
     if current_state is None:
-        text, kb = answers.user_main_menu()
+        info = answers.user_main_menu(user.lang)
     else:
         await state.clear()
-        text, kb = answers.user_main_menu(canceled=True)
+        info = answers.user_main_menu(canceled=True)
 
-    await message.answer(text, reply_markup=kb)
+    await message.answer(info.text, reply_markup=info.kb)
 
 
 @user_router.message(filters.Command(commands="start"))
 async def main(message: t.Message):
     user = await cache.get_user(message)
-    text, kb = answers.user_main_menu(lang=user.lang)
-    await message.answer(text, reply_markup=kb)
+    info = answers.user_main_menu(lang=user.lang)
+    await message.answer(info.text, reply_markup=info.kb)
 
 
 @user_router.message(F.text == "test")
